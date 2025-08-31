224743ed0ba8da8a103570c42467e9c124af3a3f
youssou975 <youssoucisse975@gmail.com>
2025-08-31 00:01:56 +0000

    CHANGEMENT DE STYLE LIVRE

--- src/components/BookSection.jsx
+++ src/components/BookSection.jsx
@@ -1,50 +1,53 @@
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import yoro from '../assets/YORO.png'; // remplace par tes vraies images
-import plastique from '../assets/plastiqur.png';
-import trace from '../assets/trace.png';
-import { motion } from "framer-motion";
+import React from "react";
+import autre from '../assets/autre.jpg';
+import soldat from '../assets/soldat.jpg';
+import noeud from '../assets/noeud.jpg';
+import gourou from '../assets/gourou.jpg';
+import camp from '../assets/camp.jpg'
+import souffle from '../assets/souffle.jpg'
+import cherif from '../assets/cherif.jpg'
+import coeur from '../assets/coeur.jpg'
 
 
-function BooksSection() {
+
+const Bookshelf = () => {
   const books = [
-    { id: 1, title: " AFRIQUE OU LA RIME PARTAGÉE", image: yoro },
-    { id: 2, title: ' MA DEMARCHE PLASTIQUE EN PARTAGE', image: plastique },
-    { id: 3, title: 'TRACE DE VIE', image: trace },
-    { id: 4, title: 'Baobab', image: yoro },
-    { id: 5, title: 'AZERTY', image: yoro },
-    { id: 6, title: 'AZERTGGFS', image: yoro },
+    { id: 1, title: "L'Autre Raison", img: autre },
+    { id: 2, title: "Le soldat inconnu mon héros", img: soldat },
+    { id: 3, title: "Les noeuds du destin", img: noeud },
+    { id: 4, title: "La république des GOUROUS", img: gourou },
+    { id: 5, title: "CAMP BOIRO", img: camp },
+    { id: 6, title: "Le souffle de Mame Mindiss ", img: souffle },
+    { id: 7, title: "Mon coeur déclame", img: coeur },
+    { id: 8, title: "Le pélerin du savoir", img: cherif },
   ];
 
+  // Découper les livres en groupes de 4
+  const chunkedBooks = [];
+  for (let i = 0; i < books.length; i += 4) {
+    chunkedBooks.push(books.slice(i, i + 4));
+  }
+
   return (
-    <motion.section id="livres" className="books-section py-5 bg-light" 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 2 }}
-      exit={{ opacity: 1 }}
-      transition={{ duration: 2 }}>
-      <Container>
-        <h2 className="text-center mb-5 section-title text-orange">Nos Livres Édités</h2>
-        <Row className="g-4">
-          {books.map((book) => (
-            <Col md={4} sm={6} key={book.id}>
-              <Card className="h-100 book-card border-0 shadow-sm">
-                <Card.Img variant="top" src={book.image} className="book-img" />
-                <Card.Body className="text-center">
-                  <Card.Title className="fw-semibold">{book.title}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-
-        <div className="text-center mt-5">
-          <Button variant="outline-warning" className="rounded-pill px-4 py-2 fw-semibold see-more-btn">
-            Voir tous les livres
-          </Button>
+    <div className="bookshelf-container">
+      <h2 className="bookshelf-title">Livres édités</h2>
+      {chunkedBooks.map((group, index) => (
+        <div key={index}>
+          <div className="shelf">
+            {group.map((book) => (
+              <div className="book" key={book.id}>
+                <img src={book.img} alt={book.title} />
+                <p>{book.title}</p>
+              </div>
+              
+            ))}
+          </div>
+          <div className="shelf-base"></div>
         </div>
-      </Container>
-    
-    </motion.section>
+      ))}
+  <button className="voir-plus">VOIR PLUS</button>
+    </div>
   );
-}
+};
 
-export default BooksSection;
+export default Bookshelf;
